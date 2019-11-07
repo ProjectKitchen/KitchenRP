5528c0442d752ae0681948511da717bb06803539
Matt Legat <legat.matt@gmail.com>
2019-11-07 14:07:25 +0100

    added basic angular components

--- kitchenRP-frontend/kitchenRP/src/app/app-routing.module.ts
+++ kitchenRP-frontend/kitchenRP/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReservationsComponent } from "./components/reservations/reservations.component";
+import {CalendarComponent} from "./components/calendar/calendar.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'calendar', component: CalendarComponent},
+    {path: 'reservations', component: ReservationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- kitchenRP-frontend/kitchenRP/src/app/app.module.ts
+++ kitchenRP-frontend/kitchenRP/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ReservationsComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
