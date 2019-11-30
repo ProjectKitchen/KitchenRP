8d4b3d0a07adb96fda513b0c34dcaf4deb2cb873
Matthias C <matts.chory@gmx.at>
2019-11-30 20:02:42 +0100

    basic my reservation list

---
+++ kitchenRP-frontend/kitchenRP/src/app/components/reservations/reservation-status-list/reservation-modal/reservation-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-reservation-modal',
+  templateUrl: './reservation-modal.component.html',
+  styleUrls: ['./reservation-modal.component.css'],
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Hi there!</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>Hello, {{name}}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+export class ReservationModalContent {
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
+
+@Component({
+  selector: 'app-reservation-modal',
+  templateUrl: './reservation-modal.component.html',
+  styleUrls: ['./reservation-modal.component.css']
+})
+export class ReservationModalComponent {
+  constructor(private modalService: NgbModal) {}
+
+  open() {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = 'World';
+  }
+}
