fa4877facb7ecdff9bb269dfe604f9f1344362d0
Matthias C <matts.chory@gmx.at>
2020-01-19 12:08:26 +0100

    improved all reservations list - fetch data and style

--- kitchenRP-frontend/kitchenRP/src/app/components/all-reservations/all-reservations-status-list/all-reservations-status-list.component.ts
+++ kitchenRP-frontend/kitchenRP/src/app/components/all-reservations/all-reservations-status-list/all-reservations-status-list.component.ts
@@ -1,107 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PipeTransform } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith, tap, flatMap } from 'rxjs/operators';
 
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {ModalReservationComponent} from "../../../modals/modal-reservation/modal-reservation.component";
 
+import {Reservation} from "../../../types/reservation";
+import {ReservationService} from "../../../services/reservation/reservation.service";
+
 @Component({
   selector: 'app-all-reservations-status-list',
   templateUrl: './all-reservations-status-list.component.html',
-  styleUrls: ['./all-reservations-status-list.component.css']
+  styleUrls: ['./all-reservations-status-list.component.css'],
+  providers: [DecimalPipe]
 })
 export class AllReservationsStatusListComponent implements OnInit {
+  pendingData: Reservation[] = [];
+  checkedData: Reservation[] = [];
 
-  // test data
-  tablePending = [
-    {
-      "id": 1,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer1",
-      "status": "Pending"
-    },{
-      "id": 2,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer2",
-      "status": "Pending"
-    },{
-      "id": 3,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer3",
-      "status": "Pending"
-    },{
-      "id": 4,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer4",
-      "status": "Pending"
-    },{
-      "id": 5,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer4",
-      "status": "Pending"
-    },{
-      "id": 6,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer4",
-      "status": "Pending"
-    }
-  ];
+  pendingReservations$: Observable<Reservation[]>;
+  checkedReservations$: Observable<Reservation[]>;
+  filter = new FormControl('');
 
-  // test data
-  tableChecked = [
-    {
-      "id": 1,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer1",
-      "status": "Accepted"
-    },{
-      "id": 2,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer2",
-      "status": "Accepted"
-    },{
-      "id": 3,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer3",
-      "status": "Denied"
-    },{
-      "id": 4,
-      "date":"11-12-2019",
-      "timeStart": "10:00",
-      "timeEnd": "12:00",
-      "duration": "2:00",
-      "resource": "Printer4",
-      "status": "Accepted"
-    }
-  ];
-  constructor(private modalService: NgbModal) { }
+  constructor(private reservationService: ReservationService, private modalService: NgbModal, pipe: DecimalPipe) {
+    this.pendingReservations$ = this.reservationService.getBy({status: "Pending"})
+        .pipe(
+            tap(reservations => this.pendingData = reservations),
+            flatMap(r => this.filter.valueChanges
+                .pipe(
+                    startWith(''),
+                    map(text => this.search(text, pipe, this.pendingData))
+                )
+            )
+        );
+
+    this.checkedReservations$ = this.reservationService.getBy({status: "Accepted,Denied"})
+        .pipe(
+            tap(reservations => this.checkedData = reservations),
+            flatMap(r => this.filter.valueChanges
+                .pipe(
+                    startWith(''),
+                    map(text => this.search(text, pipe, this.checkedData))
+                )
+            )
+        );
+  }
 
   ngOnInit() {
+  }
+
+  search(text: string, pipe: PipeTransform, data: Reservation[]): Reservation[] {
+    return data.filter(reservation => {
+    const term = text.toLowerCase();
+    return  reservation.startTime.toLowerCase().includes(term)
+        || reservation.endTime.toLowerCase().includes(term)
+        || reservation.status.toLowerCase().includes(term);
+        //|| pipe.transform(reservation.id).includes(term); // ID search?
+    });
   }
 
   openModal(tableRow) {
